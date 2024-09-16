48c4db0d249a8143835bac9b5245a280d29fa094
M4800 <vuonggthang@gmail.com>
2024-09-17 02:11:20 +0700

    commit 9/16 lan1

--- app/src/main/java/com/example/app1/LoginActivity.kt
+++ app/src/main/java/com/example/app1/LoginActivity.kt
@@ -1,17 +1,22 @@
 package com.example.app1
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.app1.databinding.ActivityLoginBinding
 import com.example.app1.model.UserModel
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.auth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -19,6 +24,7 @@ import com.google.firebase.database.database
 import java.lang.ref.Reference
 
 class LoginActivity : AppCompatActivity() {
+    private  var userName: String ?= null
     private lateinit var email:String
     private lateinit var password:String
     private lateinit var auth:FirebaseAuth
@@ -48,6 +54,7 @@ class LoginActivity : AppCompatActivity() {
 
         binding.loginButton.setOnClickListener {
             // get data from text field
+
             email = binding.emailAddress.text.toString().trim()
             password = binding.password.text.toString().trim()
             if(email.isBlank()||password.isBlank()){
@@ -62,7 +69,41 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this, SignActivity::class.java)
             startActivity(intent)
         }
+        //google sign in
+        binding.googleButton.setOnClickListener{
+            val signIntent = googleSignInClient.signInIntent
+            launcher.launch(signIntent)
+        }
     }
+    ///
+    //launcher google signin
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                if (task.isSuccessful) {
+                    val account : GoogleSignInAccount? = task.result
+                    val credential = GoogleAuthProvider.getCredential(account?.idToken,null)
+                    auth.signInWithCredential(credential).addOnCompleteListener{
+                            task ->
+                        if (task.isSuccessful){
+                            //successfully sign in with google
+//                            Toast.makeText(this,"Đăng nhập thành công với google",Toast.LENGTH_SHORT).show()
+//                            updateUi(authTask.result?.user)
+                            startActivity(Intent(this,MainActivity::class.java))
+                            finish()
+                        }else{
+                            Toast.makeText(this,"Đăng ký không thành công với google",Toast.LENGTH_SHORT).show()
+                            Log.e("GoogleSignIn", "signInResult:failed code=" + task.exception)
+                        }
+                    }
+                }
+                else{
+                    Toast.makeText(this,"Đăng ký không thành công với google",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
 
     private fun createUser() {
         auth.signInWithEmailAndPassword(email,password).addOnCompleteListener { task->
@@ -87,7 +128,7 @@ class LoginActivity : AppCompatActivity() {
         // get data from text field
         email = binding.emailAddress.text.toString().trim()
         password = binding.password.text.toString().trim()
-        val user = UserModel(email,password)
+        val user = UserModel(userName,email,password)
         val userId = FirebaseAuth.getInstance().currentUser!!.uid
         //save data
         database.child("user").child(userId).setValue(user)
